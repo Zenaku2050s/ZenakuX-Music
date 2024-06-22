32e10c29efb8ad788d6d357ff377f1b0fe8ccd48
Zenaku2050s <yusume65usm@gmail.com>
2024-06-22 17:04:41 +0630

    Add files via upload

---
+++ ZenakuX-Music/__init__.py
@@ -0,0 +1,30 @@
+from SafoneAPI import SafoneAPI
+
+from ZenakuX-Music.core.bot import VIP
+from ZenakuX-Music.core.dir import dirr
+from ZenakuX-Music.core.git import git
+from ZenakuX-Music.core.userbot import Userbot
+from ZenakuX-Music.misc import dbb, heroku
+
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = VIP()
+api = SafoneAPI()
+userbot = Userbot()
+HELPABLE = {}
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
+APP = "tg_vc_bot"  # connect music api key "Dont change it"
---
+++ ZenakuX-Music/__main__.py
@@ -0,0 +1,72 @@
+import asyncio
+import importlib
+
+from pyrogram import idle
+
+import config
+from config import BANNED_USERS
+from ZenakuX-Music import (
+    HELPABLE,
+    LOGGER,
+    app,
+    userbot,
+)
+from ZenakuX-Music.core.call import VIP
+from ZenakuX-Music.misc import sudo
+from ZenakuX-Music.plugins import ALL_MODULES
+from ZenakuX-Music.plugins.tools.clone import restart_bots
+from ZenakuX-Music.utils.database import get_banned_users, get_gbanned
+
+
+async def init():
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER(__name__).error(
+            "𝐒𝐭𝐫𝐢𝐧𝐠 𝐒𝐞𝐬𝐬𝐢𝐨𝐧 𝐍𝐨𝐭 𝐅𝐢𝐥𝐥𝐞𝐝, 𝐏𝐥𝐞𝐚𝐬𝐞 𝐅𝐢𝐥𝐥 𝐀 𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 V2 𝐒𝐞𝐬𝐬𝐢𝐨𝐧🤬"
+        )
+
+    await sudo()
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            if user_id not in BANNED_USERS:
+                BANNED_USERS.add(user_id)
+    except:
+        pass
+    await app.start()
+    for all_module in ALL_MODULES:
+        imported_module = importlib.import_module(all_module)
+
+        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+            if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
+                HELPABLE[imported_module.__MODULE__.lower()] = imported_module
+
+    LOGGER("ZenakuX-Music.plugins").info("𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 𝐁𝐚𝐛𝐲🥳...")
+
+    await userbot.start()
+
+    await VIP.start()
+    await VIP.decorators()
+    await restart_bots()
+    LOGGER("ZenakuX-Music").info("╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 𝗩𝗜𝗣 𝗕𝗢𝗬♨️\n╚═════ஜ۩۞۩ஜ════╝")
+    await idle()
+
+    await app.stop()
+    await userbot.stop()
+
+    LOGGER("ZenakuX-Music").info(
+        "                 ╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 𝗩𝗜𝗣 𝗕𝗢𝗬♨️\n╚═════ஜ۩۞۩ஜ════╝"
+    )
+
+
+if __name__ == "__main__":
+
+    asyncio.get_event_loop().run_until_complete(init())
